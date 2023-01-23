9d11294ccc8199dc830630bbc9182a9a62408c2a
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-23 10:00:50 +0100

    rugged fix of code

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -4,7 +4,7 @@ import math
 from typing import List
 
 
-def index_range(self, page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> tuple:
     """ return a tuple of size two containing a start index and an end index
         corresponding to the range of indexes to return in a list for those
         particular pagination parameters. """
