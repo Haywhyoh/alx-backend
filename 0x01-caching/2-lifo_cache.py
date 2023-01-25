bd95c59cab753d0532b3ae38203a492392f44431
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-25 17:11:28 +0100

    soory this is task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO caching is doing the thing."""
+    def __init__(self):
+        """Intialize values"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Must assign to the dictionary self.cache_data
+        the item value for the key key."""
+        if key and item:
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last = self.cache_data.popitem()
+            print("DISCARD: {}".format(last[0]))
+
+    def get(self, key):
+        """Must return the value in self.cache_data linked to key"""
+        if self.cache_data.get(key) is None or key is None:
+            return None
+        return self.cache_data.get(key)
